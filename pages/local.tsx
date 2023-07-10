97bee9762ba0811c30c60d8786c6a9a37567a7c3
Fernando Dingler <ferdingler@users.noreply.github.com>
2023-07-10 10:26:43 -0700

    Update local.tsx

--- pages/local.tsx
+++ pages/local.tsx
@@ -11,7 +11,10 @@ export default function LocalImage() {
         <title>Local Image</title>
       </Head>
       <main className={styles.main}>
+        <h1>With import from public</h1>
         <Image src={seattle} alt="Seattle" fill placeholder="blur" />
+        <h1>With src=/thom-milkovic-unsplash.jpg</h1>
+        <Image src="/thom-milkovic-unsplash.jpg" alt="Seattle" fill placeholder="blur" />
       </main>
     </>
   );
